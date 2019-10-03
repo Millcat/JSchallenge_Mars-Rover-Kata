0914b0711063c008b1b6e4a531170501064a1331
camille <camille.grison@gmail.com>
2019-10-03 20:23:30 +0200

    bonus4-second part- others rovers with struggling...

--- JSchallenge-MarsRoverKata.js
+++ JSchallenge-MarsRoverKata.js
@@ -1,16 +1,43 @@
 // Rover Object Goes Here
 // ======================
 
-const rover = {
-    direction : "N",
-    x: 0,
-    y: 0,
-    travelLog: [{ // after each move, the new coordonates will be push as an object in this array
-        x:0,
-        y:0
-        }
-    ]
-};
+const roversArray = [ // create an array with all the rovers
+
+    {
+        name: "RoverA",
+        direction : "N",
+        x: 0,
+        y: 0,
+        travelLog: [{ // after each move, the new coordonates will be push as an object in this array
+            x:0,
+            y:0
+        }]
+    },
+
+    {
+        name: "RoverB",
+        direction : "E",
+        x: 0,
+        y: 1,
+        travelLog: [{
+            x:0,
+            y:1
+        }]
+    },
+
+    {
+        name: "RoverC",
+        direction : "S",
+        x: 5,
+        y: 1,
+        travelLog: [{
+            x:5,
+            y:1
+        }]
+    }
+];
+
+let rover = roversArray[0]; // choose which rover I want to control (to not change all my code)
 
 // ====================== Obstacles // 0 means "OK go and do the move", "1" means "Obstacle! No Go"
 
@@ -27,6 +54,7 @@ const map = [
     [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
     [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
 ];
+
 
 function isInsideMap(x, y) { // variables to fix the limit of the map
     const outLeft = x < 0;
@@ -48,6 +76,7 @@ function isInsideMap(x, y) { // variables to fix the limit of the map
     // }
 }
 
+
 function isNoObstacle(x, y) {
     if (map[y][x] === 0) {                             //if there is no Obstacles...
         return true;                                   //...then you can go
@@ -57,8 +86,24 @@ function isNoObstacle(x, y) {
     }
 }
 
-function canGo(x,y) { // if isInsideMap = true and ifNoObstacles = true alors canGo = true
-    if (isInsideMap(x,y) && isNoObstacle(x,y)) {
+/* idem than isNoObstacle but instead => isNoRover.
+Each new rover change the number 0 in map when is here */
+
+function isNoRover(x, y) { // check if the coordonates of my rover are different of the others roversArray[1,2,3,...]
+    let hasRover = false;
+
+    roversArray.forEach(rover => { // for each rover in the array
+        if (rover.x === x && rover.y === y) { // if {x,y} of my rover are the same than others rovers ones
+            hasRover = true; // then THERE IS a rover and print message
+            console.log(`Can't go : there is the rover ${rover.name}`); // & name of the rover here
+        }
+        return !hasRover; // means that there is no rover !
+    });
+}
+
+
+function canGo(x,y) { // if isInsideMap = true and ifNoObstacles = true and isNoRover = true ,THEN you canGo = true
+    if (isInsideMap(x,y) && isNoObstacle(x,y) && isNoRover(x, y)) {
         return true;
     }
     return false;
@@ -67,7 +112,7 @@ function canGo(x,y) { // if isInsideMap = true and ifNoObstacles = true alors ca
 
 // ====================== to turn
 
-function turnLeft(rover) {
+function turnLeft(rover) { // with function createNewRover(), I need to modify all the Object.property
     switch (rover.direction) {
         case "N":
             rover.direction = "W";
@@ -89,6 +134,7 @@ function turnLeft(rover) {
 
 // test turnLeft function : OK
 // turnLeft(rover);
+
 
 function turnRight(rover) {
     switch (rover.direction) {
@@ -112,6 +158,7 @@ function turnRight(rover) {
 
 // test turnRight function : OK
 // turnRight(rover);
+
 
 // ====================== to move
 
@@ -159,6 +206,7 @@ function moveForward(rover) {
 // test moveForward function = OK
 // moveForward(rover);
 
+
 function moveBackward(rover) {
     let hasMoved = false;
 
@@ -204,7 +252,8 @@ function moveBackward(rover) {
 
 /* whereToMove is a parameter which is a string of letters either f,r or l.
 This parameter has to be converted to an array and each letter will be executed by an associated function(s) */
-function commands (whereToMove) {
+
+function commands (rover, whereToMove) { // command to a rover where to move
     let separateMoves = [];  // method .split() could be use instead of this and loop
     for (i = 0; i < whereToMove.length; i++) {
         let move = whereToMove.charAt(i);
@@ -230,5 +279,22 @@ function commands (whereToMove) {
 }
 
 
-//commands("frffbf"); //
+// ====================== TEST & RUN THE CODE !
+
+// 1) Move RoverA
+const myRover = roversArray[0];
+//commands(myRover, "frffbf");
+//==> [end map , turn to E, {1,0}, {2,0}, {1,0}, {2,0}]
+//==> BUT INSTEAD I HAVE only : [end map, turn to E, end of map] and no travelLog incremented :'(
+//==> None of the moveForward & moveBackward appears...
+
+// 2) Move RoverB
+const myRover = roversArray[1];
+//commands(myRover, "fflfrf");
+//==> [{1,1}, {2,1}, turn to N, meet roverA, turn to E, {3,1}]
+
+// 3) Move RoverC
+const myRover = roversArray[2];
+//commands(myRover, "rffblf");
+//==> [turn to W, {4,1}, meet roverB, {5,1}, turn to S, obstacle]
 
